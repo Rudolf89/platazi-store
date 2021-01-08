0dbadadc610e33af32e4fda70c21d60d5e220d95
rodolfo.gonzalez <rodolfo.gr89@gmail.com>
2021-01-08 00:57:28 -0300

    uso de ng if y ng for simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  items = ['Rodolfo','Luis','Angel','Nicolas','Miguel'];
+
+  addItem(){
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index:number){
+    this.items.splice(index, 1);
+  }
 }

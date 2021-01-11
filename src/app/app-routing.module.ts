96b7c4291c7d6bbb009929e576e24ca5fe8e5cd4
rodolfo.gonzalez <rodolfo.gonzalez@andes-tech.cl>
2021-01-11 13:50:03 -0300

    header banner y footer del home detalle productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     component: HomeComponent
@@ -16,12 +23,20 @@ const routes: Routes = [
     component: ProductsComponent
   },
   {
+    path: 'product/:id',
+    component: ProductDetailComponent
+  },
+  {
     path: 'contact',
     component: ContactComponent
   },
   {
     path: 'demo',
     component: DemoComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
